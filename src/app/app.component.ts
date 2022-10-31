3543f3dff95ec8c8acc2394886fc894dccf0cbfe
mahbubsabuj <mahbubul.alam@selise.ch>
2022-10-31 18:23:34 +0600

    Add auth module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'medium_clone';
+  // print(): void {
+  //   console.log(
+  //     '🚀 ~ file: app.component.ts ~ line 10 ~ AppComponent ~ title',
+  //     this.title
+  //   );
+  // }
 }
---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegisterComponent } from './register/register.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: 'register', component: RegisterComponent }];
+
+@NgModule({
+  declarations: [RegisterComponent],
+  imports: [CommonModule, RouterModule.forChild(routes)],
+})
+export class AuthModule {}
